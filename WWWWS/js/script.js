ba6c46ee9f0816447e06a22cdab4a4a9cc932835
EgorMiadel <83283745+EgorMiadel@users.noreply.github.com>
2021-06-16 18:58:27 +0300

    Add files via upload

---
+++ WWWWS/js/script.js
@@ -0,0 +1,8 @@
+//----------menu burger-----------*/
+$(document).ready(function(){
+    $('.header_burger').click(function(){
+        $('.header_burger,.header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+//----------menu burger-----------*/
