1e5c37b9217fb2e09b8210a66f9c925164b52c9a
EgorMiadel <83283745+EgorMiadel@users.noreply.github.com>
2021-09-14 16:46:07 +0300

    Delete WWWWS directory

--- WWWWS/js/script.js
+++
@@ -1,8 +0,0 @@
-//----------menu burger-----------*/
-$(document).ready(function(){
-    $('.header_burger').click(function(){
-        $('.header_burger,.header_menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
-//----------menu burger-----------*/
